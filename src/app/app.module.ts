a4da236b0be9d30a8734f805b8de78b16f9f80ed
Manu Bhardwaj <mnbhardwaj888@gmail.com>
2019-10-16 16:11:01 +0530

    [add] truncate pipe in sidebar item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {Page1Component} from './pages/page1.component';
 import {Page2Component} from './pages/page2.component';
 import {Page3Component} from './pages/page3.component';
 import {Page4Component} from './pages/page4.component';
+import {TruncatePipe} from './pipes/truncate.pipe';
 
 const routes: Route[] = [
   {path: 'page1', component: Page1Component},
@@ -35,7 +36,8 @@ const routes: Route[] = [
     Page1Component,
     Page2Component,
     Page3Component,
-    Page4Component
+    Page4Component,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
