46f4cfb306cb9848e2e1ab134964f723a95f87aa
Manu Bhardwaj <mnbhardwaj888@gmail.com>
2019-10-21 18:28:24 +0530

    [update] sidebar example

---
+++ src/app/pages/page5.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-page5',
+  template: '<p>Page5</p><router-outlet></router-outlet>',
+})
+export class Page5Component {}
---
+++ src/app/pages/page7.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-page7',
+  template: '<p>Page7</p><router-outlet></router-outlet>',
+})
+export class Page7Component {}
