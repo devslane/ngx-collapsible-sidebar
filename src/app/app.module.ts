55cf3940ab536b2500441511d1ce87aef54080e7
Manu Bhardwaj <mnbhardwaj888@gmail.com>
2019-10-21 17:09:07 +0530

    [update] example sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,20 +13,18 @@ import {User1Component} from './pages/user1.component';
 import {User2Component} from './pages/user2.component';
 import {User3Component} from './pages/user3.component';
 import {NgxCollapsibleSidebarModule} from '../../projects/ngx-collapsible-sidebar/src/lib/ngx-collapsible-sidebar.module';
+import {Page5Component} from './pages/page5.component';
+import {Page6Component} from './pages/page6.component';
+import {Page7Component} from './pages/page7.component';
 
 const routes: Route[] = [
-  {path: 'page1', component: Page1Component, children: [
-      {path: 'user1', component: User1Component},
-      {path: 'user2', component: User2Component},
-      {path: 'user3', component: User3Component}
-    ]},
+  {path: 'page1', component: Page1Component},
   {path: 'page2', component: Page2Component},
-  {path: 'page3', component: Page3Component, children: [
-      {path: 'user1', component: User1Component},
-      {path: 'user2', component: User2Component},
-      {path: 'user3', component: User3Component}
-    ]},
-  {path: 'page4', component: Page4Component, children: [
+  {path: 'page3', component: Page3Component},
+  {path: 'page4', component: Page4Component},
+  {path: 'page5', component: Page5Component},
+  {path: 'page6', component: Page6Component},
+  {path: 'page7', component: Page7Component, children: [
       {path: 'user1', component: User1Component},
       {path: 'user2', component: User2Component},
       {path: 'user3', component: User3Component}
@@ -40,6 +38,9 @@ const routes: Route[] = [
     Page2Component,
     Page3Component,
     Page4Component,
+    Page5Component,
+    Page6Component,
+    Page7Component,
     User1Component,
     User2Component,
     User3Component
